c2cbf02547e01a4e2bea49cc54ada0ee9ff44015
PavPar <paramonpav@yandex.ru>
2021-05-29 03:08:33 +0300

    forgot to commit buddy ? :P

--- server-stuff/main.py
+++ server-stuff/main.py
@@ -4,6 +4,8 @@ import pymorphy2
 from flask import Flask, request, jsonify, Response
 from pymongo import MongoClient
 from bson import json_util
+from mlxtend.frequent_patterns import apriori, fpgrowth, fpmax, association_rules
+import pandas as pd
 
 from yargy import (
     Parser,
@@ -27,10 +29,13 @@ from yargy import interpretation as interp
 
 INT = type('INT')
 
+from word2numberi18n import w2n
+import os
+
 try:
     client = MongoClient('mongodb://localhost:27017/', )
     client.server_info()
-    db = client['metroDB']
+    db = client["mainAPI"]
     print("DB CONNECTED")
     print(flask.__version__)
 except pymongo.errors.ServerSelectionTimeoutError as err:
@@ -76,27 +81,57 @@ def getCategoriesFromRequest(categoriesList, request):
     for match in parser.findall(request):
         for category in [_.value for _ in match.tokens]:
             categoryName = morph.parse(category)[0].normal_form.upper()
-            foundCategoriesNames.append(categoryName)
-
-    categoriesList[categoryName]["vendorlist"]
-
+            if categoryName not in foundCategoriesNames:
+                foundCategoriesNames.append(categoryName)
     return foundCategoriesNames
+def getVendorList(data):
+    vendorList = []
+    vendorValues = list(data.values())
+    for alt in vendorValues:
+        for value in alt:
+            vendorList.append(value)
+    return vendorList
+
+def findVendorData(vendorData,vendor):
+    for vendorName in vendorData.keys():
+        if(vendor.upper() in vendorData[vendorName]):
+            return vendorName
+    return ""
 
 def getItemData(categoryData,searchReq):
     Item = fact(
         'item',
         ['amount', 'unit','category', 'vendor']
     )
+    LITERALS = {
+        'один': 1,
+        'два': 2,
+        'три': 3,
+        'четыре': 4,
+        'пять': 5,
+        'шесть': 6,
+        'семь': 7,
+        'восемь': 8,
+        'девять': 9,
+    }
+
 
     CATEGORY = morph_pipeline([categoryData["name"]])
-    VENDOR = morph_pipeline(categoryData["vendorlist"])
-    UNIT = morph_pipeline(["буханка","буханка"])
-    AMOUNT = INT.interpretation(
+    VENDOR = morph_pipeline(getVendorList(categoryData["vendorlist"][0]))
+    UNIT = morph_pipeline(categoryData["unit"])
+    AMOUNT = dictionary(LITERALS).interpretation(
+        interp.normalized().custom(LITERALS.get)
+    )
+
+    AMOUNT_NUM = INT.interpretation(
         interp.custom(int)
     )
 
     ITEM_MISC = rule(
         AMOUNT.interpretation(
+            Item.amount
+        ).optional(),
+        AMOUNT_NUM.interpretation(
             Item.amount
         ).optional(),
         UNIT.interpretation(
@@ -112,26 +147,49 @@ def getItemData(categoryData,searchReq):
         Item
     )
 
+    morph = pymorphy2.MorphAnalyzer()
     parser = Parser(ITEM_MISC)
-    itemsData = []
+    AllItemsData = []
     for match in parser.findall(searchReq):
-        for itemData in [_.value for _ in match.tokens]:
-            itemsData.append(itemData)
-    return itemsData
+        structItemData ={
+            "vendor":findVendorData(categoryData["vendorlist"][0],match.fact.vendor),
+            "category":morph.parse(match.fact.category)[0].normal_form.upper(),
+            "amount":match.fact.amount,
+            "unit":match.fact.unit,
+        }
+        AllItemsData.append(structItemData)
+    return {categoryData["name"]: AllItemsData}
 
+
+def normalizeDF(DF):
+    DF["itemCount"] = DF.groupby(["userID", "itemID"])['categoryID'].transform('count')
+    DF = DF[DF.duplicated(keep=False)]
+    newDF = DF
+    newDF = newDF.iloc[0:0]
+    for userID in DF.userID.unique():
+        DF["itemorderfreq"] = (
+                    DF.itemCount / DF[DF.userID == userID].groupby(["userID"]).count()['itemCount'].to_list()[0])
+        temp = DF[DF.userID == userID].sort_values(by="itemorderfreq", ascending=False).head(5)
+        newDF = newDF.append(temp)
+    return newDF.drop(["itemCount", "itemorderfreq"], axis=1).reset_index()
 
 @app.route('/tokenize', methods=['POST'])
 def tokenize():
     if not request.is_json:
+        print('not a json')
         return 'not a json', 400
     reqData = request.get_json()  # get all data from request
     if not "searchReq" in reqData:
+        print('no request')
         return 'no request', 400
+    if not "categories" in reqData:
+        print('no categories')
+        return 'no categories',400
 
     categRaw = transformCursorToList(db.categories.find())  # Get raw categories from DB
 
-    allCategories = getAllcategories(categRaw)
-
+    allCategories = getAllcategories(reqData["categories"])
+    print(reqData["categories"])
     foundCategories = getCategoriesFromRequest(allCategories,reqData["searchReq"])
 
     res = []
@@ -141,5 +199,31 @@ def tokenize():
 
     return Response(
         json_util.dumps(res),
+        mimetype='application/json'
+    )
+@app.route('/recomenditem', methods=['POST'])
+def recomendItem():
+    reqData = request.get_json()  # get all data from reques
+    orderList = db["orders"]
+    data = transformCursorToList(orderList.find())
+    newData = []
+    for order in data:
+        for iteminfo in order['order']:
+            item = iteminfo['data']
+            newData.append({"userID": order["userID"], "partnerID": order["partnerID"], "itemID": item["_id"],
+                            "itemName": item["name"],"categoryID":item['category_id'], "manufacturerID": item["manufacturer"]["id"],
+                            "manufacturerName": item["manufacturer"]["name"]})
+    DF = pd.DataFrame(newData)
+    DF = normalizeDF(DF)
+    data_by_item = DF.groupby(['userID'])['itemID'].apply(','.join).reset_index()
+    data_by_item_dummies = data_by_item['itemID'].str.get_dummies(',')
+    frequent_itemsets = apriori(data_by_item_dummies, min_support=0.02, use_colnames=True)
+    frequent_itemsets.sort_values(by=['support'], ascending=False).head(1)
+    rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.1)
+    possibleItems = rules[rules["antecedents"] == {reqData["itemID"]}]
+    alternativeItems = possibleItems.sort_values(by=['lift', 'conviction'], ascending=[False, True]).head(5)
+    result = alternativeItems.explode('consequents').reset_index(drop=True)
+    return Response(
+        json_util.dumps(list(dict.fromkeys(result['consequents'].to_list()))),
         mimetype='application/json'
     )
